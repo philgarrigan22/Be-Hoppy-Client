dec990868d122c62d36a2fca2470ba7e71a94912
philgarrigan22 <philgarrigan@comcast.net>
2019-04-16 12:22:40 -0400

    Add import for font 'typeface-roboto'
    - Required as it is used by Material-UI.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/index.scss'
+import 'typeface-roboto'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import { HashRouter } from 'react-router-dom'
