d765e6aea6c72846c023b0929d927195654c3e42
philgarrigan22 <philgarrigan@comcast.net>
2019-04-18 12:51:57 -0400

    Update brewery search
    - Update SearchBreweries component to handle requests for locations that
    return no results. Added message to search in another area.

--- src/resources/breweries/components/SearchBreweries.js
+++ src/resources/breweries/components/SearchBreweries.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { withRouter } from 'react-router-dom'
 
 import { findBreweries } from '../api'
 import YelpResults from './YelpResults'
 
 import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
 
 class FindBreweries extends Component {
   constructor () {
@@ -40,49 +41,59 @@ class FindBreweries extends Component {
   }
 
   render () {
-    const { search, businesses } = this.state
+    const { search, businesses, empty } = this.state
 
-    // if (empty) {
-    //   return (
-    //     <Fragment>
-    //
-    //       <Paper>
-    //         <h2>Looks like that beer is not in our system. Search for another, or create a new review using your super secret beer.</h2>
-    //         <Button component={Link} to="/reviews-create" variant="contained" color="secondary">
-    //                   Create Review
-    //         </Button>
-    //       </Paper>
-    //     </Fragment>
-    //   )
-    // }
-    return (
-      <div className='yelp-container'>
-        <form className='find-breweries-form' onSubmit={this.submitSearch}>
-          <h3>Go out and visit your local breweries!</h3>
-          <input
-            required
-            name='search'
-            value={search}
-            type='text'
-            placeholder='Where you at?'
-            onChange={this.handleChange} />
-          <Button type="submit" variant="contained" color="primary">Search</Button>
-        </form>
-        <div className='returned-breweries'>
-          {businesses.map(brewery =>
-            <YelpResults key={brewery.id}
-              name={brewery.name}
-              imageUrl={brewery.image_url}
-              isClosed={brewery.is_closed}
-              url={brewery.url}
-              reviewCount={brewery.review_count}
-              rating={brewery.rating.toString().replace(/\./g, '')}
-              displayAddress={brewery.location.display_address}
-              displayPhone={brewery.display_phone}
-            />)}
+    if (empty) {
+      return (
+        <Fragment>
+          <div className='yelp-container'>
+            <form className='find-breweries-form' onSubmit={this.submitSearch}>
+              <h3>Go out and visit your local breweries!</h3>
+              <input
+                required
+                name='search'
+                value={search}
+                type='text'
+                placeholder='Where you at?'
+                onChange={this.handleChange} />
+              <Button type="submit" variant="contained" color="primary">Search</Button>
+            </form>
+            <Paper>
+              <h2>We are unable to pull up breweries in your area. Try searching in another area.</h2>
+            </Paper>
+          </div>
+        </Fragment>
+      )
+    } else {
+      return (
+        <div className='yelp-container'>
+          <form className='find-breweries-form' onSubmit={this.submitSearch}>
+            <h3>Go out and visit your local breweries!</h3>
+            <input
+              required
+              name='search'
+              value={search}
+              type='text'
+              placeholder='Where you at?'
+              onChange={this.handleChange} />
+            <Button type="submit" variant="contained" color="primary">Search</Button>
+          </form>
+          <div className='returned-breweries'>
+            {businesses.map(brewery =>
+              <YelpResults key={brewery.id}
+                name={brewery.name}
+                imageUrl={brewery.image_url}
+                isClosed={brewery.is_closed}
+                url={brewery.url}
+                reviewCount={brewery.review_count}
+                rating={brewery.rating.toString().replace(/\./g, '')}
+                displayAddress={brewery.location.display_address}
+                displayPhone={brewery.display_phone}
+              />)}
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
