f4264638e9a3c6895c97c6b4fdace92a6bee76b8
philgarrigan22 <philgarrigan@comcast.net>
2019-04-19 02:06:10 -0400

    Add styling to header

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,36 +3,45 @@ import { Link } from 'react-router-dom'
 
 import '../css/header/Header.scss'
 
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
 const authenticatedOptions = (
   <React.Fragment>
-    <Link to="/reviews">Reviews</Link>
-    <Link to="/yelp-search">Find Breweries</Link>
-    <Link to="/change-password">Change Password</Link>
-    <Link to="/sign-out">Sign Out</Link>
+    <Button color="inherit" component={Link} to="/reviews">Reviews</Button>
+    <Button color="inherit" component={Link} to="/yelp-search">Find Breweries</Button>
+    <Button color="inherit" component={Link} to="/change-password">Change Password</Button>
+    <Button color="inherit" component={Link} to="/sign-out">Sign Out</Button>
   </React.Fragment>
 )
 
 const unauthenticatedOptions = (
   <React.Fragment>
-    <Link to="/sign-up">Sign Up</Link>
-    <Link to="/sign-in">Sign In</Link>
+    <Button color="inherit" component={Link} to="/sign-up">Sign Up</Button>
+    <Button color="inherit" component={Link} to="/sign-in">Sign In</Button>
   </React.Fragment>
 )
 
 const alwaysOptions = (
   <React.Fragment>
-    <Link to="/">Home</Link>
+    <Button color="inherit" component={Link} to="/">Home</Button>
   </React.Fragment>
 )
 
 const Header = ({ user }) => (
-  <header className="main-header">
-    <h1>Be Hoppy</h1>
-    <nav>
-      { user && <span>Welcome, {user.email}</span>}
-      { user ? authenticatedOptions : unauthenticatedOptions }
-      { alwaysOptions }
-    </nav>
+  <header>
+    <div className="main-header">
+      <div className="nav-title">
+        <Typography variant="h3" color="inherit">
+            Be Hoppy
+        </Typography>
+      </div>
+      <div className="nav-btns">
+        { user && <span>Welcome, {user.email}</span>}
+        { user ? authenticatedOptions : unauthenticatedOptions }
+        { alwaysOptions }
+      </div>
+    </div>
   </header>
 )
 
