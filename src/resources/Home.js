32827ef7573c9e9baf0930a530cceb0ebb90c9d4
philgarrigan22 <philgarrigan@comcast.net>
2019-06-05 03:24:22 -0400

    Add and style more Home component content

--- src/resources/Home.js
+++ src/resources/Home.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Home = ({ snackBar, user }) => (
   <div className='home-container'>
-    <div className='home-welcome'>
-      <p>Welcome!</p>
-      <div className='home-content'>
-        <p>Hello</p>
-      </div>
+    <div className='home-content'>
+      <h1>Welcome to Be H<img className="hop-icon" src="https://i.imgur.com/19ID0Li.png"/>ppy!</h1>
+    </div>
+    <div className='home-content'>
+      <h3>The one stop shop for keeping track of all your favorite craft beers</h3>
+      <h2>Search or thousands of craft beers to review</h2>
+      <h2>Keep track of your favorite and least favorite beers.</h2>
+      <p>Use our review tracking system to remember which beers you want to forget, and which beers will have you coming back for more!</p>
+      <h2></h2>
     </div>
   </div>
 )
