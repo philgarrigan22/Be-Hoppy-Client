924f60e2241a6219385314f29df8369222800cae
philgarrigan22 <philgarrigan@comcast.net>
2019-06-05 00:25:11 -0400

    Add initial Home component skeleton
    - Update Home component content.
    - Add minimal styling to index.scss.

--- src/resources/Home.js
+++ src/resources/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const Home = ({ snackBar, user }) => (
-  <div className='home'>
-
-    <div>
-
+  <div className='home-container'>
+    <div className='home-welcome'>
+      <p>Welcome!</p>
+      <div className='home-content'>
+        <p>Hello</p>
+      </div>
     </div>
   </div>
 )
