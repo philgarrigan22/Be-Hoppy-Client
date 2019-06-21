1ac51e36d2936491922bc54ccf41e3db74fd77a5
philgarrigan22 <philgarrigan@comcast.net>
2019-06-21 13:46:10 -0400

    Update home component styling
    - Add unordered list.
    - Restructure home content.
    - Style for responsive small screen.

--- src/resources/Home.js
+++ src/resources/Home.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 const Home = ({ snackBar, user }) => (
   <div className='home-container'>
-    <div className='home-content'>
+    <div className='home-content-title'>
       <h1>Welcome to Be H<img className="hop-icon" src="https://i.imgur.com/19ID0Li.png"/>ppy!</h1>
     </div>
     <div className='home-content'>
-      <h3>The one stop shop for keeping track of all your favorite craft beers</h3>
-      <h2>Search or thousands of craft beers to review</h2>
-      <h2>Keep track of your favorite and least favorite beers.</h2>
-      <p>Use our review tracking system to remember which beers you want to forget, and which beers will have you coming back for more!</p>
-      <h2></h2>
+      <h2>{'Your one stop shop for keeping track of all your favorite craft beers, breweries, and everything in between!'}</h2>
+      <h3>{' Take advantage of Be Hoppy\'s features:'}</h3>
+      <ul>
+        <li>Search for thousands of craft beers to review.</li>
+        <li>Keep track of your favorite and least favorite beers.</li>
+        <li>Use our review tracking system to remember which beers you want to forget, and which beers will have you coming back for more!</li>
+      </ul>
     </div>
   </div>
 )
